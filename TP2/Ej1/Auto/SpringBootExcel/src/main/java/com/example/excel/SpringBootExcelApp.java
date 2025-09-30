f1afabf2594fa1b62ad23c0848727642dfe7952a
LonghinoEmmanuel <105024797+LonghinoEmmanuel@users.noreply.github.com>
2025-09-30 14:13:50 -0700

    Added the second part of the tutorials for point E

---
+++ TP2/Ej1/Auto/SpringBootExcel/src/main/java/com/example/excel/SpringBootExcelApp.java
@@ -0,0 +1,11 @@
+package com.example.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExcelApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExcelApp.class, args);
+    }
+}
