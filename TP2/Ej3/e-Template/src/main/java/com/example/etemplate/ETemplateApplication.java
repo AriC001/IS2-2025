7e00ae4649e025f20a00c2f8a53c7e354f597e9e
Ari Rafael Cohen <87084361+AriC001@users.noreply.github.com>
2025-10-13 11:17:24 -0300

    ej3-e init

---
+++ TP2/Ej3/e-Template/src/main/java/com/example/etemplate/ETemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.etemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ETemplateApplication.class, args);
+    }
+
+}
