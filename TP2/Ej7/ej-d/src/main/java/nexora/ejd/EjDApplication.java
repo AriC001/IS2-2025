1ad07056cf701aebef9917db9b847b60b2c34ee8
Ari Rafael Cohen <87084361+AriC001@users.noreply.github.com>
2025-10-30 20:46:21 -0300

    basic ouath working with github

---
+++ TP2/Ej7/ej-d/src/main/java/nexora/ejd/EjDApplication.java
@@ -0,0 +1,13 @@
+package nexora.ejd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjDApplication.class, args);
+	}
+
+}
---
+++ TP2/Ej7/ej-d/src/main/java/nexora/ejd/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package nexora.ejd.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController{
+
+    @GetMapping({"","/","index"})
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+}
